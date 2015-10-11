a32a814ebadf4a702a34a7f4424b3b8ab6b8719a
jbandura <j.bandura@borderstudio.eu>
2015-10-11 16:23:34 +0200

    fix typo + add endpoint for overdue payments

--- app/controllers/api/v1/payments.rb
+++ app/controllers/api/v1/payments.rb
@@ -1,6 +1,6 @@
 module API
   module V1
-    class Payments < Grape::API 
+    class Payments < Grape::API
       version 'v1'
       format :json
       formatter :json, Grape::Formatter::ActiveModelSerializers
@@ -10,24 +10,35 @@ module API
       end
 
       resource :payments do
-        desc "Return list of students"
-        params do 
+        desc "Return list of payments"
+        params do
           optional :paid, type: Boolean
+          optional :overdue, type: Boolean
         end
-        get "", root: :payments do 
-          return Payment.all if params[:paid].nil?
-          Payment.where(paid: params[:paid])
+        get "", root: :payments do
+          if params[:overdue]
+            month_nr = Date.today.strftime("%m")
+            year_nr = Date.today.strftime("%Y")
+            return Payment.where(
+              "paid = ? AND NOT (month_nr = ? AND year_nr = ?)",
+              false,
+              month_nr,
+              year_nr
+            )
+          end
+          return Payment.where(paid: params[:paid]) if params[:paid]
+          return Payment.all
         end
 
         desc "Modifiy payment"
-        params do 
+        params do
           requires :id, type:Integer
-          requires :payment, type: Hash do 
+          requires :payment, type: Hash do
             requires :paid, type: Boolean
           end
         end
 
-        put ":id", root: :payment do 
+        put ":id", root: :payment do
           Payment.find(params[:id]).update!({
             paid: params[:payment][:paid]
           })
