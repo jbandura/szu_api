751197e39805d97a0c75a296eae85e5482558ec5
jbandura <j.bandura@borderstudio.eu>
2015-10-11 11:52:17 +0200

    include group when querying for student

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :name, :surname, :city, :street, :country, :phone_nr, :accepted_terms, :student_group_id
+  attributes :id, :name, :surname, :city, :street, :country, :phone_nr, :accepted_terms
   has_many :payments, embed: :ids, include: true
+  has_one :student_group, embed: :ids, embed_in_root: true
 end
